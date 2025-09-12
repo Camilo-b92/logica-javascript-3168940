6dd8207202a5059c4e8ce6229cc6b3c142c1546b
Camilo-b92 <camilobetancourt02@gmail.com>
2025-09-11 23:51:49 -0500

    Add files via upload

--- DOM/intro-doom/app.js
+++ DOM/intro-doom/app.js
@@ -1,24 +1,19 @@
-// 1. Seleccionar los elementos
 const pantalla = document.querySelector(".pantalla");
 const btnmenos = document.querySelector(".btn-menos");
 const btnmas = document.querySelector(".btn-mas");
 
-// 2. Definir el contador inicial
 let counter = 0;
 
-// 3. Funciones
 function incrementar() {
   counter++;
   pantalla.textContent = counter;
 
-  // Cambiar color del texto
   if (counter >= 10) {
     pantalla.style.color = "red";
   } else {
     pantalla.style.color = "black";
   }
 
-  // Cambiar color de fondo
   if (counter >= 30) {
     document.body.style.backgroundColor = "yellow";
   } else {
@@ -32,14 +27,12 @@ function decrementar() {
     pantalla.textContent = counter;
   }
 
-  // Cambiar color del texto
   if (counter >= 10) {
     pantalla.style.color = "red";
   } else {
     pantalla.style.color = "black";
   }
 
-  // Cambiar color de fondo
   if (counter >= 30) {
     document.body.style.backgroundColor = "yellow";
   } else {
@@ -47,6 +40,5 @@ function decrementar() {
   }
 }
 
-// 4. Eventos
 btnmas.addEventListener("click", incrementar);
 btnmenos.addEventListener("click", decrementar);
